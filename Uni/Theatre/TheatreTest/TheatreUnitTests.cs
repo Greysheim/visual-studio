6863767f9b593218b09ad2355e0c691b6fac640e
Daniel Cranston <00ff00@gmail.com>
2016-10-14 14:52:53 +1300

    Create projects TheatreLib and TheatreTest for Theatre Solution

---
+++ Uni/Theatre/TheatreTest/TheatreUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheatreLib;
+
+namespace TheatreTest
+{
+    [TestClass]
+    public class TheatreUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
